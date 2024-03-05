f99e4efa4b5d0f15e15750a4a0055cdac5a026a9
aritrahota <aritra828207@gmail.com>
2024-03-05 21:34:27 +0530

    code files modified

--- Executable_Proj/reducer_response.py
+++ Executable_Proj/reducer_response.py
@@ -1,7 +1,6 @@
 #extract file
 import sys
 
-# Check if the correct number of arguments are passed (script name, start date, end date)
 if len(sys.argv) != 3:
     print("Usage: script.py <Start Date(dd-mm-yyyy)> <End Date(dd-mm-yyyy)>")
     sys.exit(1)
@@ -11,7 +10,7 @@ month_names = [
     "may", "june", "july", "august",
     "september", "october", "november", "december"
 ]
-# Use command-line arguments for start and end dates
+
 start_data = sys.argv[1].split('-')
 end_data = sys.argv[2].split('-')
 
